150384da417184ac26738b032ea6e267cfa7602f
ninatsiklauri <tsiklaurinino88@gmail.com>
2025-07-24 16:07:36 +0400

    feat
    .

--- src/leaderboard.ts
+++ src/leaderboard.ts
@@ -60,9 +60,7 @@ export class Leaderboard {
 
 export async function fetchAndSetLeaderboard(leaderboard: Leaderboard) {
   try {
-    const response = await fetch(
-      "https://cube-game-back.vercel.app/api/leaderboard",
-    );
+    const response = await fetch("http://localhost:3000/leaderboard");
     if (!response.ok) throw new Error("Failed to fetch leaderboard");
     const data = await response.json();
 
